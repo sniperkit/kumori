add551a5a89e3c3c96d7536ed3e344e66f22409c
Agata Przybyszewska <agata@mathmagicians.dk>
2018-05-25 00:17:07 +0200

    Gradle can run cucumber features. Features can be run from eclipse as well

---
+++ watchman/src/test/scala/FeatureRunner.scala
@@ -0,0 +1,9 @@
+import cucumber.api.CucumberOptions
+import cucumber.api.junit.Cucumber
+import org.junit.runner.RunWith
+ 
+@RunWith(classOf[Cucumber])
+@CucumberOptions(features = Array("src/test/resources"))
+class FeatureRunner {
+  
+}
